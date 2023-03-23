c07d0ff35ad461a886675e9a18cafb4c817c02cb
dREWbxKewb <drewvasqu1432@gmail.com>
2023-03-23 17:32:03 -0500

    Building backbones

--- Webpack.config.js
+++ Webpack.config.js
@@ -1,26 +1,32 @@
 const path = require('path');
 
+const dist = path.join(__dirname, '/client/dist');
+
 module.exports = {
-  mode: "development",
-  entry: path.resolve(__dirname, 'client', 'src', 'index.jsx'),
+  entry: './client/src/index.jsx',
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, 'client', 'dist')
+    filename: 'build.js',
+    path: dist,
   },
-  watch: true,
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
   module: {
     rules: [
       {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
+      {
         test: /\.(jsx|js)$/,
         exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
-          }
+        use: ['babel-loader'],
 
-        }
-      }
-    ]
-  }
-}
+      },
+    ],
+  },
+};
+
--- client/src/MainPage/App.jsx
+++ client/src/MainPage/App.jsx
@@ -4,5 +4,7 @@ function App() {
   const [count, setCount] = useState(0);
   return (
     <button onClick={() => setCount(count + 1)}>{count}</button>
-  )
+  );
 }
+
+export default App;
